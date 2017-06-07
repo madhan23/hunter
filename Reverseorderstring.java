5c83d6f6d36f4ddce54f2a54203c84fb2001972d
madhan23 <madhanraju23@gmail.com>
2017-06-07 18:40:13 +0530

    Create Reverseorderstring.java

---
+++ Reverseorderstring.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Reverseorderstring{
+    public static void main(String args[]){
+        String s;
+        Scanner in=new Scanner(System.in);
+        s=in.nextLine();
+        StringBuilder ss=new StringBuilder(s);
+     ss=ss.reverse();
+     String f=new String(ss);
+     char x[]=f.toCharArray();
+     Arrays.sort(x);
+     String rst=new String(x);
+     System.out.println(rst);} }
+     
+     
+        
+      
